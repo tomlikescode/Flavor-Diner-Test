1e3f4be47c2fd9e31357547d28af9e00d74f477d
Tom Mallen <gtmallen@gmail.com>
2023-01-23 20:40:37 -0600

    initial push

---
+++ src/pages/index.js
@@ -0,0 +1,48 @@
+import React from "react"
+import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import AllRecipes from "../components/AllRecipes"
+// import { useStaticQuery } from "gatsby"
+
+export const Head = () => (
+  <>
+    <title>Flavor Diner</title>
+    <meta name="description" content="this is home page" />
+  </>
+)
+
+export default function Home() {
+  return (
+    <Layout>
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/brick2.jpg"
+            alt="eggs"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          ></StaticImage>
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>flavor diner</h1>
+              {/* <h4>no fluff, just recipes</h4> */}
+            </div>
+          </div>
+        </header>
+        <AllRecipes />
+      </main>
+    </Layout>
+  )
+}
+
+// const query = graphql`
+//   {
+//     site {
+//       siteMetadata {
+//         title
+//         description
+//       }
+//     }
+//   }
+// `
---
+++ src/pages/recipes.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/Layout"
+import AllRecipes from "../components/AllRecipes"
+
+
+const Recipes = () => {
+  return (
+    <Layout>
+      <main className="page">
+        <AllRecipes />
+      </main>
+    </Layout>
+  )
+}
+
+export default Recipes
