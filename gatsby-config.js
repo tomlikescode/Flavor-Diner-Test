97d5c66310e4a49b71ec864545fe1b9c89cdf150
Tom Mallen <gtmallen@gmail.com>
2023-02-28 17:11:48 -0600

    Feb 28 push

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: "gatsby-plugin-html-attributes",
+      options: {
+        lang: "en",
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
